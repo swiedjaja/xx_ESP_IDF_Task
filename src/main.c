389f45a67213bc050c947f537714cc36dbb39a04
swiedjaja <swiedjaja@gmail.com>
2022-01-14 16:03:40 +0700

    remove task.h header

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ static void taskBlinkRed(void* arg)
     while (1)
     {
         gpio_set_level(LED_RED, LED_ON);
-        printf("Hello Red\n");
+        printf("%d Hello Red\n", xPortGetCoreID());
         fflush(stdout);
         vTaskDelay(100/portTICK_RATE_MS);
         gpio_set_level(LED_RED, LED_OFF);
@@ -23,7 +23,7 @@ static void taskBlinkGreen(void* arg)
     while (1)
     {
         gpio_set_level(LED_GREEN, LED_ON);
-        printf("Hello Green\n");
+        printf("%d Hello Green\n", xPortGetCoreID());
         fflush(stdout);
         vTaskDelay(100/portTICK_RATE_MS);
         gpio_set_level(LED_GREEN, LED_OFF);
@@ -34,8 +34,9 @@ static void taskBlinkGreen(void* arg)
 void app_main() {
     gpio_pinMode(LED_RED, OUTPUT);
     gpio_pinMode(LED_GREEN, OUTPUT);
-    xTaskCreate(taskBlinkRed, "BlinkRed", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-    xTaskCreate(taskBlinkGreen, "BlinkGreen", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    xTaskCreate(taskBlinkRed, "BlinkRed", configMINIMAL_STACK_SIZE+1024, NULL, 5, NULL);
+    // xTaskCreate(taskBlinkGreen, "BlinkGreen", configMINIMAL_STACK_SIZE+1024, NULL, 5, NULL);
+    xTaskCreatePinnedToCore(taskBlinkGreen, "BlinkGreen", configMINIMAL_STACK_SIZE+1024, NULL, 5, NULL,1);
     //vTaskSuspend(NULL);
 /*    
     while (1)
